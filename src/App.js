4782d4e0a8d36d3ab7aebaec37e4bbea144a37ae
RP <ryan.pieszko@outlook.com>
2022-05-02 23:43:11 +1000

    daily update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import './index.css';
 import Header from './Header';
 import Content from './Content';
 import Footer from './Footer';
--- src/Content.js
+++ src/Content.js
@@ -1,12 +1,28 @@
+import React from "react";
+import {useState} from "react";
+
 const Content = () => {
-    const handleNameChange = () => {
-        const names = ['Bob', 'Ryan', 'Will'];
-        const int = Math.floor(Math.random()*3);
-        return names[int];
-    };
+    const [items, setItems] = useState([
+        {
+            id: 1,
+            checked: false,
+            item: "One half point bag of Cadbury"
+        },
+        {
+            id: 2,
+            checked: false,
+            item: "Item 2"
+        },
+        {
+            id: 3,
+            checked: false,
+            item: "Item 3"
+        }
+    ]);
+ 
     return (
         <main>
-            <p>Hello {handleNameChange()}!</p> 
+            
         </main>
   )
 }
